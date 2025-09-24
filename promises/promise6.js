19c71a23fc6b0aa7892c12880d845b5542ad10bb
SidW111 <siddhantwainngade@gmail.com>
2025-09-24 17:33:59 +0530

    ques

--- promises/promise6.js
+++ promises/promise6.js
@@ -14,6 +14,22 @@ console.log('E');
 myAsyncFunction();
 console.log('F');
 
+async function myAsyncFunction() {
+  console.log('A');
+  await new Promise(resolve => {
+    console.log('B');
+    setTimeout(() => {
+      console.log('C');
+      resolve();
+    }, 0);
+  });
+  console.log('D');
+}
+
+console.log('E');
+myAsyncFunction();
+console.log('F');
+
 //E
 //A
 //B
