5b3e16663188583fdc3140ee70036c86ab714b9f
SidW111 <siddhantwainngade@gmail.com>
2025-10-04 20:50:51 +0530

    example.js

---
+++ example.js
@@ -0,0 +1,42 @@
+
+// greet1()
+// greet2()
+
+
+//function expression
+// const greet2 = () =>{ 
+//     console.log('hello sid from greet2')
+// }
+
+//function declaration
+// function greet1(){
+// console.log('Hello sid')
+// }
+
+
+// if(1=='1'){
+//     console.log(1=='1')
+// }
+// if(2==='2')
+// {
+//     console.log(2==='2')
+// }
+
+// console.log(typeof null)
+
+// let num = "2" + 2;
+// console.log(num);
+
+
+// if(0){
+//     console.log(0)
+// }
+
+// if([]){
+//     console.log(2)
+// }
+
+// if(1){
+//     console.log(2)
+// }
+
