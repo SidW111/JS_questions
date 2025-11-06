f934739e984b178d62366df098818041daa4b5ed
SidW111 <siddhantwainngade@gmail.com>
2025-11-06 14:51:05 +0530

    int

---
+++ IntQ.js
@@ -0,0 +1,22 @@
+// const users = [
+//   { age: 22 },
+//   { age: 23 },
+//   { age: 22 },
+//   { age: 23 },
+//   { age: 24 },
+//   { age: 25 },
+//   { age: 26 },
+//   { age: 28 },
+//   { age: 28 },
+//   { age: 28 },
+// ];
+
+
+// //solution
+
+// const sameAge = users.reduce((acc,curr)=>{
+//    acc[curr.age] = acc[curr.age] ? acc[curr.age] + 1 : 1; 
+//     return acc; 
+// },{})
+
+// console.log(sameAge)
